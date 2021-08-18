3511cb59fa33cbe99729704069fa4dd24b0bcb09
Code Sandbox <hello@codesandbox.io>
2021-08-18 19:47:31 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.43c13094fbb15356a95b1d8c7ad3f61f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab16e96515a390c45e6b61288595567",
+    "url": "https://anmol53.github.io/csb-5r2p0/index.html"
+  },
+  {
+    "revision": "5fba8757a1bc99f04012",
+    "url": "https://anmol53.github.io/csb-5r2p0/static/css/main.c1184278.chunk.css"
+  },
+  {
+    "revision": "e18bd81398273fcacc88",
+    "url": "https://anmol53.github.io/csb-5r2p0/static/js/2.302c16f0.chunk.js"
+  },
+  {
+    "revision": "28b5e57e1e89ae408eee8d8f4d692c7c",
+    "url": "https://anmol53.github.io/csb-5r2p0/static/js/2.302c16f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fba8757a1bc99f04012",
+    "url": "https://anmol53.github.io/csb-5r2p0/static/js/main.71c96e1d.chunk.js"
+  },
+  {
+    "revision": "0c93b06652d378f1a9a9",
+    "url": "https://anmol53.github.io/csb-5r2p0/static/js/runtime-main.dbaa0b93.js"
+  },
+  {
+    "revision": "f503adea3ef4dfb5abae7a40ca7e764f",
+    "url": "https://anmol53.github.io/csb-5r2p0/static/media/container.f503adea.png"
+  },
+  {
+    "revision": "89da1d4e4f6bf5dface6a91fd2f7ed94",
+    "url": "https://anmol53.github.io/csb-5r2p0/static/media/gridArea.89da1d4e.png"
+  },
+  {
+    "revision": "49dd22a4475ff605b27c88e0f78d7b0e",
+    "url": "https://anmol53.github.io/csb-5r2p0/static/media/gridCell.49dd22a4.png"
+  },
+  {
+    "revision": "dabb089f99bef386beeda3f50cd1f939",
+    "url": "https://anmol53.github.io/csb-5r2p0/static/media/gridLine.dabb089f.png"
+  },
+  {
+    "revision": "dd47aecef0971cef0941fca69745fbd1",
+    "url": "https://anmol53.github.io/csb-5r2p0/static/media/gridTrack.dd47aece.png"
+  }
+]);
